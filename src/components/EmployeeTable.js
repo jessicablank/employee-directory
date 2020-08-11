a33d14a375924c011b53a63fdc16925700a91179
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-08-10 22:09:03 -0700

    sorting by email address

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import API from "../utils/API.js";
 import TableRow from "./TableRow";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSortAlphaDown } from '@fortawesome/free-solid-svg-icons'
+import { faSortAlphaDownAlt } from '@fortawesome/free-solid-svg-icons'
 
 class EmployeeTable extends Component {
   state = {
     employees: [],
     allEmployees: [],
     order: "ascending",
+    icon :  faSortAlphaDown,
     searchText: ""
   };
 
@@ -26,9 +30,11 @@ class EmployeeTable extends Component {
       .catch((error) => console.log(error));
   };
 
-  handleNameSort = () => {
+ 
+  handleEmailSort = () => {
     this.setState({
       order: this.state.order === "ascending" ? "descending" : "ascending",
+      icon: this.state.icon === faSortAlphaDown ?  faSortAlphaDownAlt  :  faSortAlphaDown
     });
   };
 
@@ -36,19 +42,19 @@ class EmployeeTable extends Component {
     if (this.state.employees.length === 0) {
       return <h1>Please refresh the page</h1>;
     } else {
-      const sortedNames = this.state.employees.sort((a, b) => {
-        const aName = `${a.name.first} ${a.name.last}`.toLowerCase();
-        const bName = `${b.name.first} ${b.name.last}`.toLowerCase();
-        if (aName === bName) {
+      const sortedEmails = this.state.employees.sort((a, b) => {
+        const aEmail = a.email
+        const bEmail = b.email
+        if (aEmail === bEmail) {
           return 0;
         }
         if (this.state.order === "ascending") {
-          if (aName < bName) {
+          if (aEmail < bEmail) {
             return -1;
           }
           return 1;
         }
-        if (aName < bName) {
+        if (aEmail < bEmail) {
           return 1;
         }
         return -1;
@@ -60,16 +66,16 @@ class EmployeeTable extends Component {
             <tr>
               <th scope="col">Picture</th>
               <th scope="col">Name</th>
-              <th scope="col">Email</th>
+              <th scope="col" onClick={this.handleEmailSort}> Email <FontAwesomeIcon icon={this.state.icon} /></th>
               <th scope="col">State</th>
             </tr>
           </thead>
           <tbody>
-            {sortedNames.map((employee) => {
+            {sortedEmails.map((employee) => {
               return (
                 <TableRow
                   key={employee.id.value}
-                  picture={employee.picture.thumbnail}
+                  picture={employee.picture.medium}
                   name={employee.name.first + " " + employee.name.last}
                   email={employee.email}
                   state={employee.location.state}
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,6 @@ function Header() {
         <div className="jumbotron jumbotron-fluid">
             <div className="container">
                 <h1 className="display-4 text-center font-weight-bold"> EMPLOYEE DIRECTORY</h1>
-                <h4 className="text-center">Sort by First Name and Search by State</h4>
             </div>
         </div>
     );
